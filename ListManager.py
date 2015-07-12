3a96e1d9681300b9fe514a8f44066257f99ffd28
brady salz <brady.salz@gmail.com>
2015-07-12 11:54:46 -0500

    now checks entire list for redundancy has a post timer if needed (5min)

--- ListManager.py
+++ ListManager.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 
 from WebCrawler import WebCrawler
@@ -18,47 +19,41 @@ class ListManager:
         post_file = open('postlist.csv', 'rb')
 
         last_show = show_file.readlines()
-        last_show = [l.rstrip() for l in last_show]
+        show_list = [l.rstrip() for l in last_show]
 
         last_post = post_file.readlines()
-        last_post = [l.rstrip() for l in last_post]
+        post_list = [l.rstrip() for l in last_post]
 
-        show_cnt = 0
+        for show in show_list:
+            if show not in post_list:
+                curr_show = show.split(', ')
+                crawler = WebCrawler()
+                bot = RedditBot('drumcorps')
 
-        while last_show[show_cnt] not in last_post:
-            curr_show = last_show[show_cnt].split(', ')
-            crawler = WebCrawler()
-            bot = RedditBot('dcibottest')
+                recaps = crawler.get_show_recap_url(curr_show[2])
+                shows = [crawler.parse_recap_table(r) for r in recaps]
+                bodies = [bot.parse_show_to_post(s) for s in shows]
 
-            recaps = crawler.get_show_recap_url(curr_show[2])
-            shows = [crawler.parse_recap_table(r) for r in recaps]
-            bodies = [bot.parse_show_to_post(s) for s in shows]
+                single_body = bot.get_header(shows[0])
+                single_body += '\n\n'.join(bodies)
+                single_body += bot.get_legend()
+                single_body += bot.get_footer()
 
-            single_body = bot.get_header(shows[0])
-            single_body += '\n\n'.join(bodies)
-            single_body += bot.get_legend()
-            single_body += bot.get_footer()
+                with open('redditoutput.text', 'wb') as f:
+                    f.write(single_body)
 
-            with open('redditoutput.text', 'wb') as f:
-                f.write(single_body)
+                post_title = curr_show[0] + ' Scores'
 
-            post_title = curr_show[0] + ' Scores'
+                bot.post_thread(post_title, single_body)
 
-            bot.post_thread(post_title, single_body)
+                with open('logging.txt', 'ab') as log:
+                    log.write('added new show on {0}\n'.format(datetime.now()))
+                    log.write(show + '\n')
 
-            with file('postlist.csv', 'rb') as original:
-                orig = original.read()
-            with file('postlist.csv', 'wb') as modified:
-                modified.write(', '.join(curr_show) + '\n' + orig)
+                time.sleep(5*60) # reddit post timeout?
 
-            with open('logging.txt', 'ab') as log:
-                log.write('added new show on {0}\n'.format(time.time()))
-                log.write(last_show[show_cnt])
-
-            show_cnt += 1
-
-        show_file.close()
-        post_file.close()
+        with open('postlist.csv', 'wb') as p:
+            p.write('\n'.join(show_list))
 
     def update_show_list(self):
         WebCrawler().update_show_list()
