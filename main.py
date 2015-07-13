7795c126aaecc80939565365ee9fc60d4cf68ba9
Brady Salz <brady.salz@gmail.com>
2015-07-13 11:52:54 -0400

    cron works, removed extraneous logging

--- main.py
+++ main.py
@@ -9,11 +9,7 @@ import datetime
 from ListManager import ListManager
 
 
-
 updater = ListManager()
 updater.update_show_list()
 updater.diff_show_post_lists()
-
-with open('logging.txt', 'a') as f:
-    f.write("cron'd at {0}\n".format(datetime.datetime.now()))
 
